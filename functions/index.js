ba368a08daa03e4d83a10a05e54432a874fa875d
Cody <ccolburn@uco.edu>
2021-06-22 16:55:08 -0500

    fixed html tags

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
 const functions = require("firebase-functions");
 
-const auth = require("firebase-auth");
+const admin = require("firebase-admin");
 
 const serviceAccount = require("./account_key.json");
 
-auth.initializeApp({
-    credential: auth.credential.cert(serviceAccount)
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const Constant = require('./constant.js')
@@ -13,6 +13,12 @@ const Constant = require('./constant.js')
 exports.cf_updateThread = functions.https.onCall(updateThread);
 
 async function updateThread(threadInfo, context) {
+
+    if (!context.auth.token.email) {
+        if (Constant.DEV) console.log('not logged in');
+        throw new functions.https.HttpsError('unauthenticated', 'Only the poster of the thread may invoke this function');
+    }
+
     try {
         await admin.firestore().collection(Constant.collectionNames.THREADS).doc(threadInfo.threadId).updateThread(threadInfo.data);
     } catch (e) {
